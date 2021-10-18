82ea05b6eab6c28255f9c83bcd7031df63529945
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2021-10-18 17:58:06 +0200

    Complexity histograms over (simulation) time

---
+++ plot-complexity-hists.R
@@ -0,0 +1,61 @@
+t1 = 1000
+t2 = 5000
+t3 = 10000
+maxc = 15
+
+for(expt in 1:2) {
+  if(expt == 1) {
+    png("complexity-hists-directed.png", width=800, height=800)
+    df = read.csv("stats-out-blend-1-0.100-10-16-16-64-1-0-1-5000-1e+08.csv", header=T)
+  } else {
+    png("complexity-hists-undirected.png", width=800, height=800)
+    df = read.csv("stats-out-blend-0-0.100-10-16-16-64-1-0-1-5000-1e+08.csv", header=T)
+  }
+  
+  par(mfrow=c(4,4))
+  
+  hist(df$CMNInterface[df$MinDiscoveryTime < t1], xlim=c(0,maxc), xlab="Complexity", main="Without occurrence counts:\nAll structures\ndiscovered t<t1")
+  hist(df$CMNInterface[df$MinDiscoveryTime < t2], xlim=c(0,maxc), xlab="Complexity", main="All structures\ndiscovered t<t2")
+  hist(df$CMNInterface[df$MinDiscoveryTime < t3], xlim=c(0,maxc), xlab="Complexity", main="All structures\ndiscovered t<t3")
+  hist(df$CMNInterface, xlim=c(0,maxc), xlab="Complexity", main="All structures")
+  
+  if(length(df$CMNInterface[df$Size == 16 & df$MinDiscoveryTime < t1]) == 0) { plot(0,type='n',axes=FALSE,ann=FALSE) }
+  else { hist(df$CMNInterface[df$Size == 16 & df$MinDiscoveryTime < t1], xlim=c(0,maxc), xlab="Complexity", main="16-mers\ndiscovered t<t1") }
+  hist(df$CMNInterface[df$Size == 16 & df$MinDiscoveryTime < t2], xlim=c(0,maxc), xlab="Complexity", main="16-mers\ndiscovered t<t2")
+  hist(df$CMNInterface[df$Size == 16 & df$MinDiscoveryTime < t3], xlim=c(0,maxc), xlab="Complexity", main="16-mers\ndiscovered t<t3")
+  hist(df$CMNInterface[df$Size == 16], xlim=c(0,maxc), xlab="Complexity", main="All 16-mers")
+  
+  new.df = data.frame(Complexity=NULL, Time=NULL, Count=NULL)
+  for(i in seq(from=0,to=maxc)) {
+    for(j in 1:4) {
+      new.df = rbind(new.df, data.frame(Complexity = i, Time = ifelse(j == 1, t1, ifelse(j == 2, t2, ifelse(j==3, t3, 20000))), Count=0))
+    }
+  }
+  new.16.df = new.df
+  
+  for(i in 1:nrow(df)) {
+    t = df$MinDiscoveryTime[i]
+    if(t < t1) { ref = which(new.df$Complexity == df$CMNInterface[i] & new.df$Time == t1); new.df$Count[ref] = new.df$Count[ref] + df$DiscoveryCount[i]  }
+    if(t < t2) { ref = which(new.df$Complexity == df$CMNInterface[i] & new.df$Time == t2); new.df$Count[ref] = new.df$Count[ref] + df$DiscoveryCount[i]  }
+    if(t < t3) { ref = which(new.df$Complexity == df$CMNInterface[i] & new.df$Time == t3); new.df$Count[ref] = new.df$Count[ref] + df$DiscoveryCount[i]  }
+    ref = which(new.df$Complexity == df$CMNInterface[i] & new.df$Time == 20000); new.df$Count[ref] = new.df$Count[ref] + df$DiscoveryCount[i]  
+    if(df$Size[i] == 16) {
+      if(t < t1) { ref = which(new.16.df$Complexity == df$CMNInterface[i] & new.16.df$Time == t1); new.16.df$Count[ref] = new.16.df$Count[ref] + df$DiscoveryCount[i]  }
+      if(t < t2) { ref = which(new.16.df$Complexity == df$CMNInterface[i] & new.16.df$Time == t2); new.16.df$Count[ref] = new.16.df$Count[ref] + df$DiscoveryCount[i]  }
+      if(t < t3) { ref = which(new.16.df$Complexity == df$CMNInterface[i] & new.16.df$Time == t3); new.16.df$Count[ref] = new.16.df$Count[ref] + df$DiscoveryCount[i]  }
+      ref = which(new.16.df$Complexity == df$CMNInterface[i] & new.16.df$Time == 20000); new.16.df$Count[ref] = new.16.df$Count[ref] + df$DiscoveryCount[i]  
+    }
+  }
+  
+  plot(new.df$Complexity[new.df$Time == t1], new.df$Count[new.df$Time == t1], xlab="Complexity", ylab="Frequency", main="With occurrence counts:\nAll structures\ndiscovered t<t1", type="b")
+  plot(new.df$Complexity[new.df$Time == t2], new.df$Count[new.df$Time == t2], xlab="Complexity", ylab="Frequency", main="All structures\ndiscovered t<t2", type="b")
+  plot(new.df$Complexity[new.df$Time == t3], new.df$Count[new.df$Time == t3], xlab="Complexity", ylab="Frequency", main="All structures\ndiscovered t<t3", type="b")
+  plot(new.df$Complexity[new.df$Time == 20000], new.df$Count[new.df$Time == 20000], xlab="Complexity", ylab="Frequency", main="All structures", type="b")
+  
+  plot(new.16.df$Complexity[new.16.df$Time == t1], new.16.df$Count[new.16.df$Time == t1], xlab="Complexity", ylab="Frequency", main="16-mers\ndiscovered t<t1", type="b")
+  plot(new.16.df$Complexity[new.16.df$Time == t2], new.16.df$Count[new.16.df$Time == t2], xlab="Complexity", ylab="Frequency", main="16-mers\ndiscovered t<t2", type="b")
+  plot(new.16.df$Complexity[new.16.df$Time == t3], new.16.df$Count[new.16.df$Time == t3], xlab="Complexity", ylab="Frequency", main="16-mers\ndiscovered t<t3", type="b")
+  plot(new.16.df$Complexity[new.16.df$Time == 20000], new.16.df$Count[new.16.df$Time == 20000], xlab="Complexity", ylab="Frequency", main="All 16-mers", type="b")
+  
+  dev.off()
+}
